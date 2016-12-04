d5a2bbe3d8c86fb304aa900b0a571aadae0a6410
Georgi Iliev <g.iliev@ultraplay.co>
2016-12-04 14:41:13 +0200

    Changed the name of the web and web tests project due to naming collision

---
+++ TicTacToeGame/TicTacToe.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicTacToe.Web.Startup))]
+namespace TicTacToe.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
