b0a70c47aa0c82c49733649211687333cae819f4
Georgi Iliev <g.iliev@ultraplay.co>
2017-10-24 22:12:41 +0300

    DataLayer Tests initial comit

---
+++ TicTacToeGame/TicTacToe.DataLayer.Tests/ApplicationDbContextTests.cs
@@ -0,0 +1,42 @@
+﻿namespace TicTacToe.DataLayer.Tests
+{
+    using System.Data.Entity;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Models;
+
+    [TestClass]
+    public class ApplicationDbContextTests
+    {
+        [TestMethod]
+        public void ApplicationDbContext_Should_Exist()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            Assert.IsNotNull(context);
+        }
+
+        [TestMethod]
+        public void ApplicationDbContext_Create_Method_Should_Return_An_Instance_Of_ApplicationDbContext()
+        {
+            bool isValidCast = ApplicationDbContext.Create() is ApplicationDbContext;
+
+            Assert.IsNotNull(isValidCast);
+        }
+
+        [TestMethod]
+        public void ApplicationDbContext_Should_Have_IDbSet_Of_Game()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            Assert.IsTrue(context.Games is IDbSet<Game>);
+        }
+
+        [TestMethod]
+        public void ApplicationDbContext_Should_Have_IDbSet_Of_Tile()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            Assert.IsTrue(context.Tiles is IDbSet<Tile>);
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacToe.DataLayer.Tests/Data/TicTacToeDataTests.cs
@@ -0,0 +1,56 @@
+﻿namespace TicTacToe.DataLayer.Tests.Data
+{
+    using DataLayer.Data;
+    using Interfaces;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Repository;
+    using Models;
+
+    [TestClass]
+    public class TicTacToeDataTests
+    {
+        private ITicTacToeData data;
+        
+        [TestInitialize]
+        public void SetUp()
+        {
+            this.data = new TicTacToeData(ApplicationDbContext.Create());
+        }
+
+        [TestMethod]
+        public void TicTacToeData_Should_Have_ApplicationUsers_Repository()
+        {
+            bool isValidCast = data.Users is IGenericRepository<ApplicationUser>;
+
+            Assert.IsTrue(isValidCast);
+        }
+
+        [TestMethod]
+        public void TicTacToeData_Should_Have_Games_Repository()
+        {
+            bool isValidCast = data.Games is IGenericRepository<Game>;
+
+            Assert.IsTrue(isValidCast);
+        }
+
+        [TestMethod]
+        public void TicTacToeData_Should_Have_Tiles_Repository()
+        {
+            bool isValidCast = data.Tiles is IGenericRepository<Tile>;
+
+            Assert.IsTrue(isValidCast);
+        }
+
+        [TestMethod]
+        public void TicTacToeData_Should_Have_SaveChanges_Method()
+        {
+            data.SaveChanges();
+        }
+
+        [TestMethod]
+        public void TicTacToeData_Should_Have_SaveChangesAsync()
+        {
+            data.SaveChangesAsync();
+        }
+    }
+}
